31b733475ebdfd9815b6fb559c998e145f5c2714
RetrozDev <ryanbarbozapro@gmail.com>
2023-12-16 00:48:37 +0100

    Image random

--- src/components/DuckCard.jsx
+++ src/components/DuckCard.jsx
@@ -6,25 +6,30 @@ const ducks =[]
 
 function DuckCard({duck}) {
   const [isFlipped, setIsFlipped] = useState(false);
-
- const handleFlip = () => {
+  const handleFlip = () => {
     setIsFlipped(!isFlipped);
-    ducks.push(duck.name);
-
+    ducks.push(duck.imgSrc);
+  
     if (ducks.length === 2) {
       const [name1, name2] = ducks;
-
+  
       if (name1 !== name2) {
         console.log("Les cartes sont différentes");
+      } else {
+        console.log("Les cartes sont les mêmes");
+  
+        // Supprimer les éléments correspondant à name1 et name2
+        const elementsToRemove = document.querySelectorAll(`.backFace[data-name="${name1}"], .backFace[data-name="${name2}"]`);
+        
+        elementsToRemove.forEach(element => element.remove());
       }
-      else{
-        console.log("Les cartes sont les même");
-      }
+  
       ducks.length = 0;
     }
-
+  
     console.log(ducks);
   };
+  
 
 
 
--- src/components/DuckContainer.jsx
+++ src/components/DuckContainer.jsx
@@ -1,10 +1,23 @@
+import React from "react";
 import DuckCard from "./DuckCard";
 import "./DuckContainer.css";
 
+function shuffleArray(array) {
+  // Algorithme de Fisher-Yates pour mélanger le tableau
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 function DuckContainer({ duckPictures }) {
+  // Mélanger le tableau de manière aléatoire
+  const shuffledDucks = shuffleArray([...duckPictures]);
+
   return (
     <div id="duckContainer">
-      {duckPictures.map((duck, indexDuck) => (
+      {shuffledDucks.map((duck, indexDuck) => (
         <DuckCard key={indexDuck} duck={duck} />
       ))}
     </div>
