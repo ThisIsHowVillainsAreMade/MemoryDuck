056d90c29b9d44f92eb7b83e3c3da4293f3f873d
RetrozDev <ryanbarbozapro@gmail.com>
2023-12-16 00:31:44 +0100

    Ajout d'une fonction pour tester si les cartes sont les mêmess

--- src/components/DuckCard.jsx
+++ src/components/DuckCard.jsx
@@ -2,15 +2,30 @@ import { useState } from "react";
 import "./DuckCard.css";
 import CyberQuack from "../assets/CyberQuack.png";
 const ducks =[]
+
+
 function DuckCard({duck}) {
   const [isFlipped, setIsFlipped] = useState(false);
 
-  
-  const handleFlip = () => {
+ const handleFlip = () => {
     setIsFlipped(!isFlipped);
-    ducks.push(duck.id)
+    ducks.push(duck.name);
+
+    if (ducks.length === 2) {
+      const [name1, name2] = ducks;
+
+      if (name1 !== name2) {
+        console.log("Les cartes sont différentes");
+      }
+      else{
+        console.log("Les cartes sont les même");
+      }
+      ducks.length = 0;
+    }
+
     console.log(ducks);
   };
+
 
 
 
